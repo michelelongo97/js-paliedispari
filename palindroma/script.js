07d014ec027a244cda8ad2c06d213604c9933881
michelelongo97 <m.longo7x@gmail.com>
2024-12-04 13:15:44 +0100

    init project

---
+++ palindroma/script.js
@@ -0,0 +1,11 @@
+//Chiedere all’utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//Consigli del giorno
+//1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+//2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+//Domande da  farsi quando si crea una funzione:
+//1. Come dovrebbe chiamarsi?
+//2. Ho bisogno di parametri?
+//3. Devo restituire un valore?
+//4. Se sì, di che tipo?
