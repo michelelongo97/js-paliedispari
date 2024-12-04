1c2eaf131a9d3a750570c724fe31066c359605ad
michelelongo97 <m.longo7x@gmail.com>
2024-12-04 16:43:59 +0100

    feat: add first solution

--- palindroma/script.js
+++ palindroma/script.js
@@ -9,3 +9,21 @@
 //2. Ho bisogno di parametri?
 //3. Devo restituire un valore?
 //4. Se sì, di che tipo?
+
+
+// 1 - Creo una funzione che inverte le lettere della parola
+function palindroma(word) {
+    let wordReverse = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+    wordReverse = wordReverse + word[i];
+    }
+    return wordReverse;
+}
+// 2 - Chiedo all’utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+// 3 - Se la parola inserita è palindroma mostro un alert che me lo conferma, altrimenti un alert che mi dice il contrario.
+if (userWord === palindroma(userWord)) {
+    alert(`La parola inserita è palindroma`);
+} else {
+    alert(`La parola inserita non è palindroma`);
+}
